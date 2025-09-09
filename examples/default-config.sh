b968d04d623487eb562f3a639f3e22401eae3e43
Riddhiman Saha <riddhiman1014@gmail.com>
2025-09-09 14:50:53 -0400

    Change default config

--- examples/default-config.sh
+++ examples/default-config.sh
@@ -2,4 +2,7 @@
 REMOTE="lynx"
 REMOTE_ON_SITE="saha@lynx.dfci.harvard.edu"
 RSYNC_EXCLUDES+=(".venv" "__pycache__")
+REMOTE_RUN_DIR="tmp/MAIC"
+RESTORE_CMD='Rscript -e "if (requireNamespace(\"renv\", quietly=TRUE)) renv::restore()"'  # safe if renv absent
+RUN_CMD='R --quiet -e "source(\"run.R\"); save.image(\"remote_env.RData\")" --args __RUN_ARGS__'
 
