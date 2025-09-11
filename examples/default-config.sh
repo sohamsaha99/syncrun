efdf825e9a0ee2ed1059291eb038832e2800714e
Riddhiman Saha <riddhiman1014@gmail.com>
2025-09-11 15:01:43 -0400

    No hardcoded remote data file

--- examples/default-config.sh
+++ examples/default-config.sh
@@ -3,6 +3,7 @@ REMOTE="lynx"
 REMOTE_ON_SITE="saha@lynx.dfci.harvard.edu"
 RSYNC_EXCLUDES+=(".venv" "__pycache__")
 REMOTE_RUN_DIR="tmp/MAIC"
+REMOTE_DATA="remote_env.RData"
 RESTORE_CMD='Rscript -e "if (requireNamespace(\"renv\", quietly=TRUE)) renv::restore()"'  # safe if renv absent
 RUN_CMD='R --quiet -e "source(\"run.R\"); save.image(\"remote_env.RData\")" --args __RUN_ARGS__'
 
